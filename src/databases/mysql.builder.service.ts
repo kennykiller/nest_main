ea87e51894fdc2d83b938b5f56ceb6ebcf27bdfc
Vadim Aktsenin <vadim.aktsenin@1win.pro>
2025-05-11 23:23:57 +0300

    feat: filter OR added
    subconditions are necessary to add in following commits

--- src/databases/mysql.builder.service.ts
+++ src/databases/mysql.builder.service.ts
@@ -5,7 +5,13 @@ import {
   PercentageTypes,
   SqlSelectOptions,
   ISqlBuilderResult,
+  SqlConditionType,
 } from './interfaces/mysql.builder.interface';
+
+interface IWhereClause {
+  clause: string;
+  conditionType: SqlConditionType;
+}
 
 @Injectable()
 export class MySqlBuilderService implements ISqlBuilder {
@@ -29,7 +35,7 @@ export class MySqlBuilderService implements ISqlBuilder {
   ): ISqlBuilderResult {
     const setClauses = Object.keys(updateData).map((key) => `${key} = ?`);
     const setParams = Object.values(updateData);
-    const whereClauses: string[] = [];
+    const whereClauses: Array<IWhereClause> = [];
     const whereParams: any[] = [];
 
     this.whereConstructor(where, whereClauses, whereParams);
@@ -37,7 +43,7 @@ export class MySqlBuilderService implements ISqlBuilder {
     const query = `
       UPDATE ${table}
       SET ${setClauses.join(', ')}
-      WHERE ${whereClauses.join(' AND ')}
+      WHERE ${this.whereStringJoiner(whereClauses)}
     `;
 
     const params = [...setParams, ...whereParams];
@@ -53,11 +59,11 @@ export class MySqlBuilderService implements ISqlBuilder {
     let query = `SELECT * FROM ${table}`;
 
     const params: any[] = [];
-    const whereClauses: string[] = [];
+    const whereClauses: Array<IWhereClause> = [];
 
     if (where) {
       this.whereConstructor(where, whereClauses, params);
-      query += ` WHERE ${whereClauses.join(' AND ')}`;
+      query += ` WHERE ${this.whereStringJoiner(whereClauses)}`;
     }
 
     if (options) {
@@ -80,13 +86,13 @@ export class MySqlBuilderService implements ISqlBuilder {
   }
 
   buildDelete(table: string, where: SqlWhereConditions): ISqlBuilderResult {
-    const whereClauses: string[] = [];
+    const whereClauses: Array<IWhereClause> = [];
     const params: any[] = [];
     this.whereConstructor(where, whereClauses, params);
 
     const query = `
       DELETE FROM ${table}
-      WHERE ${whereClauses.join(' AND ')}
+      WHERE ${this.whereStringJoiner(whereClauses)};
     `;
 
     return { query, params };
@@ -94,94 +100,194 @@ export class MySqlBuilderService implements ISqlBuilder {
 
   whereConstructor(
     whereObject: SqlWhereConditions,
-    whereClausesArray: string[],
+    whereClausesArray: Array<IWhereClause>,
     params: any[],
   ) {
     for (const key in whereObject) {
       switch (key) {
         case 'equal':
-          Object.entries(whereObject[key]).forEach(([column, value]) => {
-            whereClausesArray.push(`${column} = ?`);
-            params.push(value);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subObject]: [
+              SqlConditionType,
+              Record<string, any>,
+            ]) =>
+              Object.entries(subObject).forEach(([column, value]) => {
+                whereClausesArray.push({
+                  clause: `${column} = ?`,
+                  conditionType,
+                });
+                params.push(value);
+              }),
+          );
           break;
         case 'like':
           Object.entries(whereObject[key]).forEach(
             ([percentType, data]: [PercentageTypes, any]) => {
               if (data) {
-                Object.entries(data).forEach(([column, value]) => {
-                  whereClausesArray.push(`${column} LIKE ?`);
-                  const percentedValue =
-                    percentType === 'right'
-                      ? `${value}%`
-                      : percentType === 'left'
-                        ? `%${value}`
-                        : `%${value}%`;
-                  params.push(percentedValue);
-                });
+                Object.entries(data).forEach(
+                  ([conditionType, subObject]: [
+                    SqlConditionType,
+                    Record<string, any>,
+                  ]) =>
+                    Object.entries(subObject).forEach(([column, value]) => {
+                      whereClausesArray.push({
+                        clause: `${column} LIKE ?`,
+                        conditionType,
+                      });
+                      const percentedValue =
+                        percentType === 'right'
+                          ? `${value}%`
+                          : percentType === 'left'
+                            ? `%${value}`
+                            : `%${value}%`;
+                      params.push(percentedValue);
+                    }),
+                );
               }
             },
           );
           break;
         case 'lt':
-          Object.entries(whereObject[key]).forEach(([column, value]) => {
-            whereClausesArray.push(`${column} < ?`);
-            params.push(value);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subObject]: [
+              SqlConditionType,
+              Record<string, any>,
+            ]) =>
+              Object.entries(subObject).forEach(([column, value]) => {
+                whereClausesArray.push({
+                  clause: `${column} < ?`,
+                  conditionType,
+                });
+                params.push(value);
+              }),
+          );
           break;
         case 'lte':
-          Object.entries(whereObject[key]).forEach(([column, value]) => {
-            whereClausesArray.push(`${column} <= ?`);
-            params.push(value);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subObject]: [
+              SqlConditionType,
+              Record<string, any>,
+            ]) =>
+              Object.entries(subObject).forEach(([column, value]) => {
+                whereClausesArray.push({
+                  clause: `${column} <= ?`,
+                  conditionType,
+                });
+                params.push(value);
+              }),
+          );
           break;
         case 'gt':
-          Object.entries(whereObject[key]).forEach(([column, value]) => {
-            whereClausesArray.push(`${column} > ?`);
-            params.push(value);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subObject]: [
+              SqlConditionType,
+              Record<string, any>,
+            ]) =>
+              Object.entries(subObject).forEach(([column, value]) => {
+                whereClausesArray.push({
+                  clause: `${column} > ?`,
+                  conditionType,
+                });
+                params.push(value);
+              }),
+          );
           break;
         case 'gte':
-          Object.entries(whereObject[key]).forEach(([column, value]) => {
-            whereClausesArray.push(`${column} >= ?`);
-            params.push(value);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subObject]: [
+              SqlConditionType,
+              Record<string, any>,
+            ]) =>
+              Object.entries(subObject).forEach(([column, value]) => {
+                whereClausesArray.push({
+                  clause: `${column} >= ?`,
+                  conditionType,
+                });
+                params.push(value);
+              }),
+          );
           break;
         case 'in':
-          Object.entries(whereObject[key]).forEach(([column, value]) => {
-            if (!Array.isArray(value)) {
-              throw new BadRequestException('SYNTAX_ERROR');
-            }
-            const questionMarksForIn = value.reduce(
-              (acc) => `${acc ? `${acc}, ?` : `?`}`,
-              '',
-            );
-            whereClausesArray.push(`${column} IN (${questionMarksForIn})`);
-            params.push(...value);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subObject]: [
+              SqlConditionType,
+              Record<string, any[]>,
+            ]) =>
+              Object.entries(subObject).forEach(([column, value]) => {
+                if (!Array.isArray(value)) {
+                  throw new BadRequestException(
+                    `Field "${column}" expects an array for "IN" condition`,
+                  );
+                }
+                const questionMarksForIn = value.reduce(
+                  (acc) => `${acc ? `${acc}, ?` : `?`}`,
+                  '',
+                );
+                whereClausesArray.push({
+                  clause: `${column} IN (${questionMarksForIn})`,
+                  conditionType,
+                });
+                params.push(...value);
+              }),
+          );
           break;
         case 'between':
-          Object.entries(whereObject[key]).forEach(([column, value]) => {
-            if (!Array.isArray(value) || value.length !== 2) {
-              throw new BadRequestException(
-                `Field "${column}" expects an array for "IN" condition`,
-              );
-            }
-            whereClausesArray.push(`${column} BETWEEN ? AND ?`);
-            params.push(...value);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subObject]: [
+              SqlConditionType,
+              Record<string, [any, any]>,
+            ]) =>
+              Object.entries(subObject).forEach(([column, value]) => {
+                if (!Array.isArray(value) || value.length !== 2) {
+                  throw new BadRequestException(
+                    `Field "${column}" expects an array for "BETWEEN" condition`,
+                  );
+                }
+                whereClausesArray.push({
+                  clause: `${column} BETWEEN ? AND ?`,
+                  conditionType,
+                });
+                params.push(...value);
+              }),
+          );
           break;
         case 'isNull':
-          whereObject[key].forEach((column) => {
-            whereClausesArray.push(`${column} IS NULL`);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subArray]: [SqlConditionType, string[]]) =>
+              subArray.forEach((column) => {
+                whereClausesArray.push({
+                  clause: `${column} IS NULL`,
+                  conditionType,
+                });
+              }),
+          );
           break;
         case 'isNotNull':
-          whereObject[key].forEach((column) => {
-            whereClausesArray.push(`${column} IS NOT NULL`);
-          });
+          Object.entries(whereObject[key]).forEach(
+            ([conditionType, subArray]: [SqlConditionType, string[]]) =>
+              subArray.forEach((column) => {
+                whereClausesArray.push({
+                  clause: `${column} IS NOT NULL`,
+                  conditionType,
+                });
+              }),
+          );
           break;
       }
     }
+  }
+
+  whereStringJoiner(whereClauseArray: IWhereClause[]): string {
+    return whereClauseArray.reduce((acc, cur, idx) => {
+      if (!idx) {
+        acc = cur.clause;
+      } else if (cur.conditionType === 'and') {
+        acc = `${acc} AND ${cur.clause}`;
+      } else {
+        acc = `${acc} OR ${cur.clause}`;
+      }
+
+      return acc;
+    }, '');
   }
 }
